d08ead8d5d5786376a37d2d33a4583f50c53a7ad
Aqua Strikr <69995597+AquaStrikr@users.noreply.github.com>
2022-03-28 00:39:34 -0300

    Update healthCounter.lua

--- health-counter/healthCounter.lua
+++ health-counter/healthCounter.lua
@@ -1,9 +1,40 @@
+local Counter_Size = 25 -- default: 25
+local Position = 'up' -- positions: left, up, right
+
+------------------------------------------------------------------------------------------
+
 function onCreate()
-	makeLuaText('counter',math.floor(getProperty('health') * 500 / 10) .. '%',0,950,634)
-	setTextSize('counter', 25)
-	addLuaText('counter')
+	if not downscroll then
+		if Position == 'right' then
+			makeLuaText('counter',getProperty('healthBar.percent') .. '%',0,950,635)
+			setTextSize('counter', Counter_Size)
+			addLuaText('counter')
+		elseif Position == 'up' then
+			makeLuaText('counter',getProperty('healthBar.percent') .. '%',0,640,550)
+			setTextSize('counter', Counter_Size)
+			addLuaText('counter')
+		elseif Position == 'left' then
+			makeLuaText('counter',getProperty('healthBar.percent') .. '%',0,275,635)
+			setTextSize('counter', Counter_Size)
+			addLuaText('counter')
+		end
+	elseif downscroll then
+		if Position == 'right' then
+			makeLuaText('counter',getProperty('healthBar.percent') .. '%',0,950,70)
+			setTextSize('counter', Counter_Size)
+			addLuaText('counter')
+		elseif Position == 'left' then
+			makeLuaText('counter',getProperty('healthBar.percent') .. '%',0,275,70)
+			setTextSize('counter', Counter_Size)
+			addLuaText('counter')
+		elseif Position == 'up' then
+			makeLuaText('counter',getProperty('healthBar.percent') .. '%',0,640,10)
+			setTextSize('counter', Counter_Size)
+			addLuaText('counter')
+		end
+	end
 end
 
 function onUpdate()
-	setTextString('counter',math.floor(getProperty('health') * 500 / 10) .. '%')
+	setTextString('counter',getProperty('healthBar.percent') .. '%')
 end
