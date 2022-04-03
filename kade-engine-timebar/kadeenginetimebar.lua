b49d7505551aaf194df6aab32b185383ee105bb5
Aqua Strikr <69995597+AquaStrikr@users.noreply.github.com>
2022-04-03 07:37:36 -0300

    among s

---
+++ kade-engine-timebar/kadeenginetimebar.lua
@@ -0,0 +1,12 @@
+function onUpdatePost(el)
+    setProperty('timeBar.color', getColorFromHex('1CFF00'))
+    setProperty('timeBarBG.color', getColorFromHex('00000'))
+    setProperty('timeBarBG.alpha', 1)
+    setProperty('timeBar.alpha', 1)
+    setProperty('timeBar.x', 180)
+    setProperty('timeBarBG.x', 180)
+    setProperty('timeTxt.visible', false)
+	makeLuaText('funnikade', getPropertyFromClass('PlayState', 'SONG.song') .. ' ' .. getProperty('') .. '',getProperty('healthBarBG.width'),200 ,20)
+	addLuaText('funnikade')
+setTextSize('funnikade', 30)
+end
